12ef6739bfa7893a3454cf8a2aeb8fee80f06232
Ben Healey <bentastic27@gmail.com>
2023-03-10 21:40:17 -0500

    fix password setting

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 adduser ${USERNAME}
-echo "${PASSWORD}\n${PASSWORD}" | smbpasswd -a -s ${USERNAME}
+echo -e "${PASSWORD}\n${PASSWORD}" | smbpasswd -a -s ${USERNAME}
 usermod -g users ${USERNAME}
 unset PASSWORD
 exec "$@"
