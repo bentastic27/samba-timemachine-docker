41e0f132deaee7e6f7976e8bc706f195093da6ba
Ben Healey <bentastic27@gmail.com>
2023-09-06 12:04:41 -0400

    unset tm_max_size for now

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,4 @@
 useradd -M ${USERNAME} -G users
 echo -e "${PASSWORD}\n${PASSWORD}" | smbpasswd -a -s ${USERNAME}
 unset PASSWORD
-
-CMD=("${@}")
-CMD+=("--option=\"fruit:time machine max size\"=${TM_MAX_SIZE+500G}")
-
-exec "$CMD"
+exec "$@"
