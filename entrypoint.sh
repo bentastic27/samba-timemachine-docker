f94dc7402cde16172219e647f06dd9c82bdff292
Ben Healey <bentastic27@gmail.com>
2023-09-06 11:57:03 -0400

    fix entrypoint and added sha tag to image

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,8 @@
 useradd -M ${USERNAME} -G users
 echo -e "${PASSWORD}\n${PASSWORD}" | smbpasswd -a -s ${USERNAME}
 unset PASSWORD
-exec "$@ --option=\"fruit:time machine max size\"=${TM_MAX_SIZE+500G}"
+
+CMD=("${@}")
+CMD+=("--option=\"fruit:time machine max size\"=${TM_MAX_SIZE+500G}")
+
+exec "$CMD"
